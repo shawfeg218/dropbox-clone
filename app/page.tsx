4fd19015699697676e5556024475e907c360637c
shawn <shawnfeng610@gmail.com>
2023-12-15 21:22:56 +0800

    modified index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,47 @@
-import { UserButton } from "@clerk/nextjs";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Dropbox clone</h1>
-      <UserButton />
+    <main className="">
+      <div className="flex flex-col lg:flex-row items-center bg-[#1E1919] dark:bg-slate-800">
+        <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-4xl font-bold">
+            Welcome to Dropbox.
+            <br />
+            <br />
+            Storing everthing for you and your business needs. All in one place.
+          </h1>
+
+          <p className="pb-20">
+            Enhance your personal storage with Dropbox, offering a simple and efficient way to
+            upload, organize, and access files from anywhere. Securely store important documents and
+            media, and experience the convenience of easy file management and sharing in one
+            centralized solution.
+          </p>
+
+          <Link href="/dashboard" className="flex bg-blue-500 p-5 w-fit rounded-sm">
+            Try for free!
+            <ArrowRight className="ml-8" />
+          </Link>
+        </div>
+
+        {/* video */}
+        <div className="bg-[#1E1919] dark:bg-slate-800 h-full p-10">
+          <video
+            autoPlay
+            loop
+            muted
+            className="rounded-lg"
+            src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4"
+          ></video>
+        </div>
+      </div>
+
+      <p className="text-center font-bold text-xl pt-5">Disclaimer</p>
+      <p className="text-center font-light p-2">
+        This is a practice project, not a real Dropbox. It is not intended for production use.
+      </p>
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,22 +8,20 @@ export default function Header() {
     <header className="flex items-center justify-between">
       {/* left */}
       <Link href="/" className="flex items-center space-x-2">
-        <div className="bg-[#0160FE] w-fit p-2">
-          <Image alt="logo" src="/img/dropbox_icon.png" className="invert" width={40} height={40} />
+        <div className="bg-[#0160FE] w-fit p-3">
+          <Image alt="logo" src="/img/dropbox_icon.png" className="invert" width={50} height={50} />
         </div>
-        <h1 className="font-bold text-xl">Dropbox</h1>
+        <h1 className="font-bold text-2xl">Dropbox</h1>
       </Link>
 
       {/* right */}
-      <div className="flex p-4 space-x-2 items-center">
-        {/* Theme toggler */}
+      <div className="flex p-3 space-x-2 items-center">
         <ThemeToggler />
-
         <UserButton afterSignOutUrl="/" />
 
         <SignedOut>
           <SignInButton afterSignInUrl="/dashboard" mode="modal">
-            sign in
+            Sign in
           </SignInButton>
         </SignedOut>
       </div>
